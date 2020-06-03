2c14bab58c2ea8e565475691e8e4cea63602623a
YuWei Chou <demoyuw@gmail.com>
2020-06-03 14:07:56 +0800

    Switch api to use flask_restful

--- build/flask/api.py
+++ build/flask/api.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask import Response
 from flask import jsonify
 from flask import request as flask_req
+from flask_restful import Resource, Api
 import logging
 from logging import handlers
 import json
@@ -10,35 +11,50 @@ import api.util as util
 import api.auth as auth
 
 app = Flask(__name__)
+api = Api(app)
 
-@app.route("/")
-def index():
-    return jsonify({"message": "DevOps api is working"})
+ut = util.util()
+au = auth.auth()
 
-@app.route("/gitrepository", methods=['GET'])
-def gitRepository():
-    headers = {
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer {0}'.format(au.get_token(logger))
-    }
-    if flask_req.method == 'GET':
+handler = handlers.TimedRotatingFileHandler(
+    'devops-api.log', when='D'\
+        , interval=1, backupCount=14)
+handler.setFormatter(logging.Formatter(
+    '%(asctime)s %(filename)s [line:%(lineno)d] %(levelname)s %(message)s'\
+        , '%Y %b %d, %a %H:%M:%S'))
+logger = logging.getLogger('devops.api')
+logger.setLevel(logging.DEBUG)
+logger.addHandler(handler)
+
+headers = {
+    'Content-Type': 'application/json',
+    'Authorization': 'Bearer {0}'.format(au.get_token(logger))
+}
+
+class Index(Resource):
+    def get(self):
+        return {"message": "DevOps api is working"}
+
+
+class GitRepository(Resource):
+
+    def get(self):
         url = "https://10.50.1.55/v3/projects/c-7bl58:p-wxgdj/sourcecoderepositories"
 
         output = ut.callgetapi(url, logger, headers)
-        return jsonify(output.json()['data'])
+        return output.json()['data']
 
-@app.route("/pipelines", methods=['GET', 'POST'])
-def pipelines():
-    headers = {
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer {0}'.format(au.get_token(logger))
-    }
-    url = "https://10.50.1.55/v3/projects/c-7bl58:p-wxgdj/pipelines"
-    if flask_req.method == 'GET':
+
+class Pipelines(Resource):
+    
+    def get(self):
+        url = "https://10.50.1.55/v3/projects/c-7bl58:p-wxgdj/pipelines"
         # get hook project list
         output = ut.callgetapi(url, logger, headers)
-        return jsonify(output.json()['data'])
-    elif flask_req.method == 'POST':
+        return output.json()['data']
+    
+    def post(self):
+        url = "https://10.50.1.55/v3/projects/c-7bl58:p-wxgdj/pipelines"
         parameter = {
             "type": "pipeline",
             "sourceCodeCredentialId": "user-j8mp5:p-wxgdj-gitlab-root",
@@ -48,21 +64,18 @@ def pipelines():
             "triggerWebhookTag": False
         }
         output = ut.callpostapi(url, parameter, logger, headers)
-        return jsonify(output.json())
-    else:
-        return "API method not POST or GET"
+        return output.json()
 
-@app.route("/pipelines/<pipelineid>", methods=['POST', 'DELETE'])
-def pipeline(pipelineid):
-    headers = {
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer {0}'.format(au.get_token(logger))
-    }
-    url = "https://10.50.1.55/v3/projects/c-7bl58:p-wxgdj/pipelines/{0}".format(pipelineid)
-    if flask_req.method == 'DELETE':
+
+class PipelineID(Resource):
+    
+    def delete(self, pipelineid):
+        url = "https://10.50.1.55/v3/projects/c-7bl58:p-wxgdj/pipelines/{0}".format(pipelineid)
         output = ut.calldeleteapi(url, logger, headers)
         return "Successful"
-    elif flask_req.method == 'POST':
+    
+    def post(self, pipelineid):
+        url = "https://10.50.1.55/v3/projects/c-7bl58:p-wxgdj/pipelines/{0}".format(pipelineid)
         logger.info("flask_req.data")
         parameter = {"branch": "master"}
         url = url+"?action=run"
@@ -71,57 +84,39 @@ def pipeline(pipelineid):
         logger.info("headers {0}".format(headers))
         output = ut.callpostapi(url, parameter, logger,headers)
         return "successful"
-    else:
-        return "API method not DELETE"
 
-@app.route("/pipelines/<pipelineid>/branches", methods=['GET'])
-def get_pipeline_branchs(pipelineid):
-    headers = {
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer {0}'.format(au.get_token(logger))
-    }
-    url = "https://10.50.1.55/v3/projects/c-7bl58:p-wxgdj/pipelines/{0}/branches".format(pipelineid)
-    if flask_req.method == 'GET':
+
+class Get_pipeline_branchs(Resource):
+
+    def get(self, pipelineid):
+        url = "https://10.50.1.55/v3/projects/c-7bl58:p-wxgdj/pipelines/{0}/branches".format(pipelineid)
         output = ut.callgetapi(url, logger, headers)
-        return jsonify(output.json())
-    else:
-        return "API method not get"
+        return output.json()
 
-@app.route("/pipelineexecutions", methods=['GET'])
-def pipelineExecutions():
-    headers = {
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer {0}'.format(au.get_token(logger))
-    }
-    if flask_req.method == 'GET':
+class PipelineExecutions(Resource):
+
+    def get(self):
         url = "https://10.50.1.55/v3/projects/c-7bl58:p-wxgdj/pipelineexecutions?order=desc"
 
         output = ut.callgetapi(url, logger, headers)
-        return jsonify(output.json()['data'])
+        return output.json()['data']
 
-@app.route("/pipelineexecutions/<pipelineexecutionsid>", methods=['GET'])
-def pipelineExecutionsOne(pipelineexecutionsid):
-    headers = {
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer {0}'.format(au.get_token(logger))
-    }
-    if flask_req.method == 'GET':
+
+class PipelineExecutionsOne(Resource):
+
+    def get(self, pipelineexecutionsid):
         url = "https://10.50.1.55/v3/projects/c-7bl58:p-wxgdj/pipelineexecutions/{0}".format(pipelineexecutionsid)
 
         output = ut.callgetapi(url, logger, headers)
-        return jsonify(output.json()['stages'])
+        return output.json()['stages']
+
+api.add_resource(Index, '/')
+api.add_resource(GitRepository, '/gitrepository')
+api.add_resource(Pipelines, '/pipelines')
+api.add_resource(PipelineID, '/pipelines/<pipelineid>')
+api.add_resource(Get_pipeline_branchs, '/pipelines/<pipelineid>/branches')
+api.add_resource(PipelineExecutions, '/pipelineexecutions')
+api.add_resource(PipelineExecutionsOne, '/pipelineexecutions/<pipelineexecutionsid>')
 
 if __name__ == "__main__":
-    handler = handlers.TimedRotatingFileHandler(
-        'devops-api.log', when='D'\
-            , interval=1, backupCount=14)
-    handler.setFormatter(logging.Formatter(
-        '%(asctime)s %(filename)s [line:%(lineno)d] %(levelname)s %(message)s'\
-            , '%Y %b %d, %a %H:%M:%S'))
-    logger = logging.getLogger('devops.api')
-    logger.setLevel(logging.DEBUG)
-    logger.addHandler(handler)
-    ut = util.util()
-    au = auth.auth()
     app.run(host='0.0.0.0', port=10009)
-    logger.info("API_IP: {0}".format(API_IP))
