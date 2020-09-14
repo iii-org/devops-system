ffc658a9681a0b634e1a0313c08ec52e05ca8c87
YuWei Chou <demoyuw@gmail.com>
2020-09-14 16:11:11 +0800

    Format output data in API GET http://{{Http_URL}}/pipelines/rd/logs?repository_id={{repository_id}}&pipelines_exec_run={{pipline_run_time}}

--- build/devops_api/apis/resources/rancher.py
+++ build/devops_api/apis/resources/rancher.py
@@ -34,32 +34,55 @@ class Rancher(object):
         pipelineexecutions_output = util.callgetapi(self, url, logger,
                                                     headersandtoken)
         output_array = pipelineexecutions_output.json()['data']
-        logger.info ("get_rancher_pipelineexecutions output: {0}".format(output_array))
+        # logger.info ("get_rancher_pipelineexecutions output: {0}".format(output_array))
         return output_array
 
     def get_rancher_pipelineexecutions_logs(self, app, logger, ci_project_id, ci_pipeline_id,\
         pipelines_exec_run, rancher_token):
-        output_dict={}
+        output_dict = []
         headersandtoken = "Authorization: Bearer {0}".format(rancher_token)
         pipelineexecutions_output = Rancher.get_rancher_pipelineexecutions(self, app, logger, ci_project_id, ci_pipeline_id,\
         rancher_token)
         for pipelineexecution_output in pipelineexecutions_output:
             if pipelines_exec_run == pipelineexecution_output['run']:
-                for index, stage in enumerate(pipelineexecution_output['pipelineConfig'][
-                        'stages']):
-                    tmp_step_message = []
+                for index, stage in enumerate(
+                        pipelineexecution_output['pipelineConfig']['stages']):
+                    tmp_step_message = {}
                     for stepindex, step in enumerate(stage['steps']):
-                        ws = websocket.WebSocket(sslopt={"cert_reqs": ssl.CERT_NONE})
+                        ws = websocket.WebSocket(
+                            sslopt={"cert_reqs": ssl.CERT_NONE})
                         url = "wss://{0}/{1}/project/{2}/pipelineExecutions/{3}-{4}/log?stage={5}&step={6}"\
                             .format(app.config['RANCHER_IP_PORT'], app.config['RANCHER_API_VERSION'], \
                             ci_project_id, ci_pipeline_id, pipelines_exec_run, index, stepindex)
                         logger.info("wss url: {0}".format(url))
                         ws.connect(url, header=[headersandtoken])
                         result = ws.recv()
-                        logger.info("Received :'%s'" % result)
-                        tmp_step_message.append(result)
+                        # logger.info("Received :'%s'" % result)
+                        step_datail = pipelineexecution_output['stages'][
+                            index]['steps'][stepindex]
+                        if 'state' in step_datail:
+                            tmp_step_message[stepindex] = {
+                                "state": step_datail['state'],
+                                "message": result
+                            }
+                        else:
+                            tmp_step_message[stepindex] = {
+                                "state": None,
+                                "message": result
+                            }
                         ws.close()
-                    output_dict[stage['name']]= tmp_step_message
+                    stage_state = pipelineexecution_output['stages'][index]
+                    if 'state' in stage_state:
+                        output_dict.append({
+                            "name": stage['name'],
+                            "state": stage_state['state'],
+                            "steps": tmp_step_message
+                        })
+                    else:
+                        output_dict.append({
+                            "name": stage['name'],
+                            "state": None,
+                            "steps": tmp_step_message
+                        })
         logger.debug("output_dict: {0}".format(output_dict))
-
         return output_dict
