3d3869eeab868eebe38823c588d1a36e51b7b01e
Romulus Urakagi Tsai <urakagi@gmail.com>
2020-09-29 17:51:55 +0800

    Fix bug with paths without scheme

--- build/devops_api/apis/resources/redmine.py
+++ build/devops_api/apis/resources/redmine.py
@@ -1,8 +1,11 @@
-import requests, json, logging
-import werkzeug
-from flask_restful import reqparse
-from flask import send_file
 from io import BytesIO
+
+import json
+import logging
+import requests
+import werkzeug
+from flask import send_file
+from flask_restful import reqparse
 
 # from model import db, Project_relationship
 # from .util import util
