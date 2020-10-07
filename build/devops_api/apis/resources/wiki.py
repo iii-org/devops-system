357e798595d533bb1a2e324840b86c9272d2a2c6
Romulus Urakagi Tsai <urakagi@gmail.com>
2020-10-07 10:07:20 +0800

    Handle delete 404 issue

--- build/devops_api/apis/resources/wiki.py
+++ build/devops_api/apis/resources/wiki.py
@@ -10,9 +10,9 @@ class Wiki(object):
         project_plugin_relation = Project.get_project_plugin_relation(
             logger, project_id)
         redmine_key = Redmine.get_redmine_key(self, logger, app)
-        wiki_list, statu_code = Redmine.redmine_get_wiki_list(
+        wiki_list, status_code = Redmine.redmine_get_wiki_list(
             self, logger, app, project_plugin_relation['plan_project_id'])
-        if statu_code == 200:
+        if status_code == 200:
             return {"message": "success", "data": wiki_list.json()}, 200
         else:
             return {"message": "get redmine wiki list error"}, 401
@@ -21,10 +21,10 @@ class Wiki(object):
         project_plugin_relation = Project.get_project_plugin_relation(
             logger, project_id)
         redmine_key = Redmine.get_redmine_key(self, logger, app)
-        wiki_list, statu_code = Redmine.redmine_get_wiki(
+        wiki_list, status_code = Redmine.redmine_get_wiki(
             self, logger, app, project_plugin_relation['plan_project_id'],
             wiki_name)
-        if statu_code == 200:
+        if status_code == 200:
             return {"message": "success", "data": wiki_list.json()}, 200
         else:
             return {"message": "get redmine wiki error"}, 401
@@ -33,12 +33,12 @@ class Wiki(object):
         project_plugin_relation = Project.get_project_plugin_relation(
             logger, project_id)
         redmine_key = Redmine.get_redmine_key(self, logger, app)
-        wiki_list, statu_code = Redmine.redmine_put_wiki(
+        wiki_list, status_code = Redmine.redmine_put_wiki(
             self, logger, app, project_plugin_relation['plan_project_id'],
             wiki_name, args)
-        if statu_code == 204:
+        if status_code == 204:
             return {"message": "update wiki success"}, 200
-        elif statu_code == 201:
+        elif status_code == 201:
             return {"message": "create wiki success"}, 200
         else:
             return {"message": "put redmine wiki error"}, 401
@@ -47,11 +47,11 @@ class Wiki(object):
         project_plugin_relation = Project.get_project_plugin_relation(
             logger, project_id)
         redmine_key = Redmine.get_redmine_key(self, logger, app)
-        wiki_list, statu_code = Redmine.redmine_delete_wiki(
+        wiki_list, status_code = Redmine.redmine_delete_wiki(
             self, logger, app, project_plugin_relation['plan_project_id'],
             wiki_name)
         logger.debug("wiki_list: {0}".format(wiki_list))
-        if statu_code == 204:
+        if status_code == 204:
             return {"message": "success"}, 200
         else:
             return {"message": "delete redmine wiki error"}, 401
