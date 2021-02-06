3ad5616e598a8bc94bc2854632e932a261c7371d
YuWei Chou <demoyuw@gmail.com>
2021-02-06 12:23:24 +0800

    Finish [GET] /template API, to list all system can use template.

--- apis/resources/template.py
+++ apis/resources/template.py
@@ -11,10 +11,18 @@ def tm_get_template():
     gl = Gitlab(config.get("GITLAB_BASE_URL"), private_token=config.get("GITLAB_PRIVATE_TOKEN"))
     group = gl.groups.get("iii-org-app", all=True)
     for project in group.projects.list():
-        print(project)
+        pj = gl.projects.get(project.id)
+        f_raw = pj.files.raw(file_path="README.md", ref = 'main')
+        summary = {"h1": [], "h2": []}
+        for line in f_raw.decode().split('\n'):
+            if line.count("#", 0, 5) == 2:
+                summary["h2"].append(line[2:])
+            elif line.count("#", 0, 5) == 1:
+                summary["h1"].append(line[1:])
         output.append({"name": project.name, 
                        "path": project.path_with_namespace, 
-                       "version": project.tag_list})
+                       "version": project.tag_list,
+                       "summary": summary})
     return output
 
 class SingleTemplate(Resource):
