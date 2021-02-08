41436aca9238cdfcbd10d4ac06caa58703cf07c1
YuWei Chou <demoyuw@gmail.com>
2021-02-08 15:22:15 +0800

    Bug fixed. get tag detail and default branch.

--- apis/resources/template.py
+++ apis/resources/template.py
@@ -10,18 +10,28 @@ def tm_get_template():
     output = []
     gl = Gitlab(config.get("GITLAB_BASE_URL"), private_token=config.get("GITLAB_PRIVATE_TOKEN"))
     group = gl.groups.get("iii-org-app", all=True)
-    for project in group.projects.list():
-        pj = gl.projects.get(project.id)
-        f_raw = pj.files.raw(file_path="README.md", ref = 'main')
+    for group_project in group.projects.list():
+        pj = gl.projects.get(group_project.id)
+        # get default branch name
+        default_branch = "master"
+        for branch in pj.branches.list():
+            if branch.default == True:
+                default_branch = branch.name
+        # get all tag
+        tag_list = []
+        for tag in pj.tags.list():
+            tag_list.append({"name": tag.name, "commit_id": tag.commit["id"], 
+                             "commit_time":tag.commit["committed_date"]})
+        f_raw = pj.files.raw(file_path="README.md", ref = default_branch)
         summary = {"h1": [], "h2": []}
         for line in f_raw.decode().split('\n'):
             if line.count("#", 0, 5) == 2:
                 summary["h2"].append(line[2:])
             elif line.count("#", 0, 5) == 1:
                 summary["h1"].append(line[1:])
-        output.append({"name": project.name, 
-                       "path": project.path_with_namespace, 
-                       "version": project.tag_list,
+        output.append({"name": pj.name, 
+                       "path": pj.path_with_namespace, 
+                       "version": tag_list,
                        "summary": summary})
     return output
 
