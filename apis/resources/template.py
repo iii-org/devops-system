30f567378cf139d0544f6ff3cbc687ef78489dc4
YuWei Chou <demoyuw@gmail.com>
2021-02-24 16:39:03 +0800

    API [GET] http://{{server_origin}}/template/{{git_project_id}} and project create api based on iiidevops/pipeline_settings.json

--- apis/resources/template.py
+++ apis/resources/template.py
@@ -22,17 +22,11 @@ template_replace_dict = {
     "harbor.host": config.get("HARBOR_EXTERNAL_BASE_URL").replace("https://", ""),
     "git.host": config.get("GITLAB_BASE_URL").replace("http://", "")
     }
+gitlab_private_token = config.get("GITLAB_PRIVATE_TOKEN")
+gl = Gitlab(config.get("GITLAB_BASE_URL"), private_token=gitlab_private_token)
 
-template_user_option = ["db.username", "db.password", "db.name"]
 
-
-def __tm_get_git_pipline_json(repository_id, tag_name):
-    gl = Gitlab(config.get("GITLAB_BASE_URL"), private_token=config.get("GITLAB_PRIVATE_TOKEN"))
-    pj = gl.projects.get(repository_id)
-    pipe_yaml_file_name = ".rancher-pipeline.yaml"
-    for item in  pj.repository_tree():
-        if item["path"] == ".rancher-pipeline.yml":
-            pipe_yaml_file_name = ".rancher-pipeline.yml"
+def __tm_get_tag_info(pj, tag_name):
     tag_info_dict = {"tag_name": None, "commit_time": sys.float_info.max, "commit_id": None}
     tags = pj.tags.list()
     if len(tags) !=0:
@@ -52,14 +46,41 @@ def __tm_get_git_pipline_json(repository_id, tag_name):
                     tag_info_dict["commit_id"] = tag.commit["id"]
     else:
         tag_info_dict = {"tag_name": pj.default_branch, "commit_time": sys.float_info.max, "commit_id": pj.default_branch}
+    return tag_info_dict
+
+
+def __tm_get_pipe_yamlfile_name(pj, tag_name):
+    pipe_yaml_file_name = ".rancher-pipeline.yaml"
+    tag_info_dict = __tm_get_tag_info(pj, tag_name)
+    for item in  pj.repository_tree(ref=tag_info_dict["commit_id"]):
+        if item["path"] == ".rancher-pipeline.yml":
+            pipe_yaml_file_name = ".rancher-pipeline.yml"
+    return pipe_yaml_file_name
+
+def __tm_get_git_pipline_json(pj, tag_name):
+    pipe_yaml_file_name = __tm_get_pipe_yamlfile_name(pj, tag_name)
+    tag_info_dict = __tm_get_tag_info(pj, tag_name)
     f_raw = pj.files.raw(file_path = pipe_yaml_file_name, ref = tag_info_dict["commit_id"])
     pipe_json = yaml.safe_load(f_raw.decode())
-    return pipe_json, tag_info_dict, pipe_yaml_file_name
+    return pipe_json
 
+
+def __tm_read_pipe_set_json(pj, tag_name=None):
+    pip_set_json = {}
+    if tag_name is None:
+        iiidevops_folder = pj.repository_tree(path="iiidevops")
+    else:
+        tag_info_dict = __tm_get_tag_info(pj, tag_name)
+        iiidevops_folder = pj.repository_tree(path="iiidevops", ref = tag_info_dict["commit_id"])
+    for file in iiidevops_folder:
+        if file["name"] == "pipeline_settings.json":
+            f_raw = pj.files.raw(file_path="iiidevops/pipeline_settings.json", 
+                                    ref = pj.default_branch)
+            pip_set_json = json.loads(f_raw.decode())
+    return pip_set_json
 
 def tm_get_template_list():
     output = []
-    gl = Gitlab(config.get("GITLAB_BASE_URL"), private_token=config.get("GITLAB_PRIVATE_TOKEN"))
     group = gl.groups.get("iiidevops-templates", all=True)
     for group_project in group.projects.list():
         pj = gl.projects.get(group_project.id)
@@ -68,73 +89,43 @@ def tm_get_template_list():
         for tag in pj.tags.list():
             tag_list.append({"name": tag.name, "commit_id": tag.commit["id"], 
                              "commit_time":tag.commit["committed_date"]})
-        iiidevops_folder = pj.repository_tree(path="iiidevops")
-        pip_set_json = {}
-        for file in iiidevops_folder:
-            if file["name"] == "pipeline_settings.json":
-                f_raw = pj.files.raw(file_path="iiidevops/pipeline_settings.json", 
-                                     ref = pj.default_branch)
-                pip_set_json = json.loads(f_raw.decode())
+        pip_set_json = __tm_read_pipe_set_json(pj)
         output.append({"id": pj.id,
                        "name": pj.name, 
                        "path": pj.path,
                        "display": pj.name if "name" not in pip_set_json else pip_set_json["name"],
                        "description": 
                            "" if "description" not in pip_set_json else pip_set_json["description"],
-                        "version": tag_list})
+                       "version": tag_list})
     return output
 
 
 def tm_get_template(repository_id, tag_name):
-    pipe_json, tag_info_dict, pipe_yaml_file_name = __tm_get_git_pipline_json(repository_id, tag_name)
-    output = {"template_id": int(repository_id), "tag_name": tag_info_dict["tag_name"], "template_param": []}
-    for stage in pipe_json["stages"]:
-        output_dict = {}
-        output_dict["name"] = stage["name"]
-        if "when" in stage:
-            output_dict["branchs"] = stage["when"]["branch"]["include"]
-        if "steps" in stage:
-            for step in stage["steps"]:
-                for fun_key, fun_value in step.items():
-                    if "when" == fun_key:
-                        output_dict["branchs"] = fun_value["branch"]["include"]
-                    elif "applyAppConfig" == fun_key:
-                        for ans_key, ans_value in fun_value["answers"].items():
-                            if ans_key in template_user_option:
-                                output_dict[ans_key] = ans_value
-                    elif "envFrom" == fun_key:
-                        pass
-                    else:
-                        for key, value in fun_value.items():
-                            if key in template_user_option:
-                                output_dict[key] = value
-        output["template_param"].append(output_dict)
+    pj = gl.projects.get(repository_id)
+    tag_info_dict = __tm_get_tag_info(pj, tag_name)
+    pip_set_json= __tm_read_pipe_set_json(pj, tag_name)
+    output = {"id": int(repository_id), "tag_name": tag_info_dict["tag_name"]}
+    if "arguments" in pip_set_json:
+        output["arguments"] = pip_set_json["arguments"]
     return output
 
 
-def tm_use_template_push_into_pj(template_repository_id, user_repository_id, tag_name, db_username, db_password, db_name):
-    pipe_json, tag_info_dict, pipe_yaml_file_name = __tm_get_git_pipline_json(template_repository_id, tag_name)
-    gitlab_private_token = config.get("GITLAB_PRIVATE_TOKEN")
-    gl = Gitlab(config.get("GITLAB_BASE_URL"), private_token=gitlab_private_token)
-    
+def tm_use_template_push_into_pj(template_repository_id, user_repository_id, tag_name, arguments):
     template_pj = gl.projects.get(template_repository_id)
     temp_http_url = template_pj.http_url_to_repo
     secret_temp_http_url = temp_http_url[:7] + f"root:{gitlab_private_token}@" + temp_http_url[7:]
-
+    pipe_json = __tm_get_git_pipline_json(template_pj, tag_name)
+    tag_info_dict= __tm_get_tag_info(template_pj, tag_name)
+    pipe_yaml_file_name = __tm_get_pipe_yamlfile_name(template_pj, tag_name)
+    pip_set_json= __tm_read_pipe_set_json(template_pj, tag_name)
+    
     pj = gl.projects.get(user_repository_id)
     pj_http_url = pj.http_url_to_repo
     secret_pj_http_url = pj_http_url[:7] + f"root:{gitlab_private_token}@" + pj_http_url[7:]
+
     Path("pj_push_template").mkdir(exist_ok=True)
     subprocess.call(['git', 'clone', '--branch', tag_info_dict["tag_name"], secret_temp_http_url
                      , f"pj_push_template/{pj.path}"])
-    git_user_email_proc = subprocess.Popen(['git', 'config', 'user.email'], stdout=subprocess.PIPE, shell=False)
-    git_user_name_proc = subprocess.Popen(['git', 'config', 'user.name'], stdout=subprocess.PIPE, shell=False)
-    git_user_email = git_user_email_proc.stdout.read().decode("utf-8")
-    git_user_name = git_user_name_proc.stdout.read().decode("utf-8")
-    if git_user_email == "":
-        subprocess.call(['git', 'config', '--global', 'user.email', '"system@iiidevops.org"'], cwd=f"pj_push_template/{pj.path}")
-    if git_user_name == "":
-        subprocess.call(['git', 'config', '--global', 'user.name', '"system"'], cwd=f"pj_push_template/{pj.path}")
     pipe_json = None
     with open(f'pj_push_template/{pj.path}/{pipe_yaml_file_name}') as file:
         pipe_json = yaml.safe_load(file)
@@ -144,16 +135,18 @@ def tm_use_template_push_into_pj(template_repository_id, user_repository_id, tag
                     for fun_key, fun_value in step.items():
                         # Replace System parameters, like harbor.host, registry.
                         if fun_key == "applyAppConfig":
-                            for ans_key in  fun_value["answers"].keys():
+                            for ans_key in fun_value["answers"].keys():
                                 if ans_key in template_replace_dict:
                                     fun_value["answers"][ans_key] = template_replace_dict[ans_key]
-                                # Replace user input parameter.
-                                if db_username is not None and ans_key == "db.username":
-                                    fun_value["answers"][ans_key] = db_username
-                                if db_password is not None and ans_key == "db.password":
-                                    fun_value["answers"][ans_key] = db_password
-                                if db_name is not None and ans_key == "db.name":
-                                    fun_value["answers"][ans_key] = db_name
+                                # Replace by pipeline_settings.json default value
+                                for argument in pip_set_json["arguments"]:
+                                    if "default_value" in argument and argument["key"] == ans_key:
+                                        fun_value["answers"][ans_key] = argument["default_value"]
+                                # Replace by user input parameter.
+                                if arguments is not None and ans_key in arguments:
+                                    for arg_key, arg_value in arguments.items():
+                                        if arg_key is not None and ans_key == arg_key:
+                                            fun_value["answers"][ans_key] = arg_value
                         elif fun_key == "envFrom":
                             pass
                         else:
@@ -162,6 +155,14 @@ def tm_use_template_push_into_pj(template_repository_id, user_repository_id, tag
                                     fun_value[parm_key] = template_replace_dict[parm_key]
     with open(f'pj_push_template/{pj.path}/{pipe_yaml_file_name}', 'w') as file:
         documents = yaml.dump(pipe_json, file)
+    git_user_email_proc = subprocess.Popen(['git', 'config', 'user.email'], stdout=subprocess.PIPE, shell=False)
+    git_user_name_proc = subprocess.Popen(['git', 'config', 'user.name'], stdout=subprocess.PIPE, shell=False)
+    git_user_email = git_user_email_proc.stdout.read().decode("utf-8")
+    git_user_name = git_user_name_proc.stdout.read().decode("utf-8")
+    if git_user_email == "":
+        subprocess.call(['git', 'config', '--global', 'user.email', '"system@iiidevops.org"'], cwd=f"pj_push_template/{pj.path}")
+    if git_user_name == "":
+        subprocess.call(['git', 'config', '--global', 'user.name', '"system"'], cwd=f"pj_push_template/{pj.path}")
     subprocess.call(['git', 'branch'], cwd=f"pj_push_template/{pj.path}")
     shutil.rmtree(f'pj_push_template/{pj.path}/.git')
     subprocess.call(['git', 'init'], cwd=f"pj_push_template/{pj.path}")
@@ -196,11 +197,8 @@ class SingleTemplate(Resource):
         parser = reqparse.RequestParser()
         parser.add_argument('user_repository_id', type=int)
         parser.add_argument('tag_name', type=str)
-        parser.add_argument('db_username', type=str)
-        parser.add_argument('db_password', type=str)
-        parser.add_argument('db_name', type=str)
+        parser.add_argument('arguments', type=dict)
         args = parser.parse_args()
         return tm_use_template_push_into_pj(repository_id, args["user_repository_id"], 
-                                  args["tag_name"], args["db_username"], 
-                                  args["db_password"], args["db_name"])
+                                  args["tag_name"], args["arguments"])
     '''
