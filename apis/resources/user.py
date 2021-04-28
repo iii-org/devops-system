878ac86fc93169befe39084485b745266f14656f
wuchihhang <wuchihhang@iii.org.tw>
2021-04-29 00:58:05 +0800

    * Bug fixed ignore QA in list_project_members when login user is not QA or Admin

--- apis/resources/user.py
+++ apis/resources/user.py
@@ -633,6 +633,10 @@ def user_list_by_project(project_id, args):
 
     arr_ret = []
     for user_role_by_project in ret_users:
+        if get_jwt_identity() and \
+            get_jwt_identity()['role_id'] not in [role.ADMIN.id, role.QA.id] and \
+                user_role_by_project.role_id == role.QA.id:
+            continue
         user_json = NexusUser(user_role_by_project.User.id).to_json(with_projects=False)
         user_json['role_id'] = user_role_by_project.role_id
         user_json['role_name'] = role.get_role_name(user_role_by_project.role_id)
