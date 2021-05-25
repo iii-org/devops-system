a1f29d9392f9d433e40e616808b9f3f9664879fe
YuWei Chou <demoyuw@gmail.com>
2021-05-25 17:49:08 +0800

    Add collection detail into [GET] /quality/{{repository_id}}/collection_list API

--- apis/resources/quality.py
+++ apis/resources/quality.py
@@ -5,9 +5,11 @@ from .gitlab import gitlab
 import util as util
 
 paths = [{
+    "software_name": "Postman",
     "path": "iiidevops/postman",
     "file_name_key": "postman_collection.json"
 }, {
+    "software_name": "SideeX",
     "path": "iiidevops/sideex",
     "file_name_key": "sideex.json"
 }]
@@ -16,10 +18,16 @@ paths = [{
 class PostmanJSON:
     def __init__(self, input_dict):
         self.info = input_dict.get("info")
+        self.item = input_dict.get("item")
 
 class PostmanJSONInfo:
     def __init__(self, info_dict):
         self.name = info_dict.get("name")
+
+
+class PostmanJSONItem:
+    def __init__(self, item_dict):
+        self.name = item_dict.get("name")
 
 
 class SideeXJSON:
@@ -30,12 +38,24 @@ class SideeXJSON:
 class SideeXJSONSuite:
     def __init__(self, input_dict):
         self.title = input_dict.get("title")
+        self.cases = input_dict.get("cases")
+
+
+class SideeXJSONCase:
+    def __init__(self, input_dict):
+        self.title = input_dict.get("title")
+        self.records = input_dict.get("records")
+
+
+class SideeXJSONRecord:
+    def __init__(self, record_dict):
+        self.name = record_dict.get("name")
 
 
 def qu_get_collection_list(repository_id):
     out_dict = {}
     for path in paths:
-        out_dict[path["file_name_key"]] = []
+        out_dict[path["software_name"]] = []
         trees = gitlab.ql_get_collection(repository_id, path['path'])
         for tree in trees:
             if path["file_name_key"] in tree["name"]:
@@ -43,15 +63,27 @@ def qu_get_collection_list(repository_id):
                 coll_json = json.loads(gitlab.gl_get_file(repository_id, path_file))
                 if path["file_name_key"] == "postman_collection.json":
                     collection_obj = PostmanJSON(coll_json)
-                    postman_info_ob = PostmanJSONInfo(collection_obj.info)
-                    out_dict[path["file_name_key"]].append({"file_name": tree["name"], "name": postman_info_ob.name})
+                    postman_info_obj = PostmanJSONInfo(collection_obj.info)
+                    items = []
+                    for item in collection_obj.item:
+                        postman_item_obj = PostmanJSONItem(item)
+                        items.append({"name": postman_item_obj.name})
+                    out_dict[path["software_name"]].append({"file_name": tree["name"], "name": postman_info_obj.name, "items": items})
                 elif path["file_name_key"] == "sideex.json":
                     suits_list = []
                     sideex_obj = SideeXJSON(coll_json)
                     for suite_dict in sideex_obj.suites:
-                        suite = SideeXJSONSuite(suite_dict)
-                        suits_list.append(suite.title)
-                    out_dict[path["file_name_key"]].append({"file_name": tree["name"], "names": suits_list})
+                        cases= []
+                        suite_obj = SideeXJSONSuite(suite_dict)
+                        for case_dict in  suite_obj.cases:
+                            records = []
+                            case_obj = SideeXJSONCase(case_dict)
+                            for record_dict in case_obj.records:
+                                record_obj = SideeXJSONRecord(record_dict)
+                                records.append({"name": record_obj.name})
+                            cases.append({"title": case_obj.title, "records": records})
+                        suits_list.append({"title": suite_obj.title, "cases": cases})
+                    out_dict[path["software_name"]].append({"file_name": tree["name"], "suites": suits_list})
     return out_dict
 
 def qu_get_collection(repository_id, software_name, collection_name):
