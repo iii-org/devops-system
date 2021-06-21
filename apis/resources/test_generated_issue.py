c249ea0a27bb124227ef4c8e52419f337e221cc2
Romulus Urakagi Tsai <urakagi@gmail.com>
2021-06-21 12:25:36 +0800

    Done auto issue

--- apis/resources/test_generated_issue.py
+++ apis/resources/test_generated_issue.py
@@ -11,11 +11,14 @@ def tgi_feed_postman(row):
     for col_key, result in collections.items():
         assertions = result.get('assertions')
         if assertions.get('failed') > 0:
-            _create_or_update_issue(row.project_id, 'postman',
-                                    col_key, _get_postman_issue_description(row))
+            _handle_test_failed(row.project_id, 'postman',
+                                col_key, _get_postman_issue_description(row))
+        else:
+            _handle_test_success(row.project_id, 'postman',
+                                 col_key, _get_postman_issue_close_description(row))
 
 
-def _create_or_update_issue(project_id, software_name, filename, description):
+def _handle_test_failed(project_id, software_name, filename, description):
     relation_row = model.IssueCollectionRelation.query.filter_by(
         software_name='postman',
         file_name=filename
@@ -40,9 +43,31 @@ def _create_or_update_issue(project_id, software_name, filename, description):
                     and j.user.id != 1):  # User id 1 means Redmine admin == system operation
                 # Do nothing
                 return
+        # Check if is previously closed (by the system). If so, reopen it.
+        if iss.status.id == 6:
+            iss.status_id = 1
         desc = iss.description
         iss.description = desc + '\n' + description
         iss.save()
+
+
+def _handle_test_success(project_id, software_name, filename, description):
+    relation_row = model.IssueCollectionRelation.query.filter_by(
+        software_name='postman',
+        file_name=filename
+    ).first()
+    if relation_row is None:
+        # No fail issue, nothing to do
+        return
+    issue_id = relation_row.issue_id
+    iss = redmine_lib.redmine.issue.get(issue_id)
+    if iss.status.id == 6:
+        # Already closed, nothing to do
+        return
+    iss.status_id = 6
+    desc = iss.description
+    iss.description = desc + '\n' + description
+    iss.save()
 
 
 def tgi_create_issue(args, software_name, file_name):
@@ -61,3 +86,7 @@ def tgi_create_issue(args, software_name, file_name):
 
 def _get_postman_issue_description(row):
     return f'{row.branch} #{row.commit_id} Postman 自動化測試失敗 ({row.total - row.fail}/{row.total})'
+
+
+def _get_postman_issue_close_description(row):
+    return f'{row.branch} #{row.commit_id} Postman 自動化測試成功 ({row.total})'
