fb705c2ae71bbed6a2020b62b682c60067fe198e
YuWei Chou <demoyuw@gmail.com>
2021-07-20 12:15:29 +0800

    feat (template): API /project/{{git_project_id}}/pipeline/default_branch add plugin filter.

--- apis/resources/template.py
+++ apis/resources/template.py
@@ -558,6 +558,7 @@ def tm_put_pipeline_branches(repository_id, data):
 
 
 def tm_get_pipeline_default_branch(repository_id):
+    __add_plugin_soft_status()
     pj = gl.projects.get(repository_id)
     if __check_git_project_is_empty(pj):
         return {}
@@ -583,7 +584,8 @@ def tm_get_pipeline_default_branch(repository_id):
                     ":")[1].replace("iii-dev-charts3-", "")
             for software in support_software:
                 if catalogTemplate_value is not None and software[
-                        "template_key"] == catalogTemplate_value:
+                        "template_key"] == catalogTemplate_value and \
+                        software.get("plugin_disabled") is False:
                     stage_out_list["name"] = software["display"]
                     stage_out_list["key"] = software["template_key"]
                     if "when" in stage:
