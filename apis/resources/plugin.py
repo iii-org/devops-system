edb9844d8c2cdc182d52efe8fa76a439fa332438
Romulus Urakagi Tsai <urakagi@gmail.com>
2021-08-03 11:37:48 +0800

    Remove redundant code

--- apis/resources/plugin.py
+++ apis/resources/plugin.py
@@ -11,7 +11,6 @@ from resources import role
 invalid_plugin_name = 'Unable get plugin software'
 
 
-
 class Plugins(Resource):
     @jwt_required
     def get(self):
@@ -33,22 +32,6 @@ class Plugin(Resource):
         args = parser.parse_args()
         plugins.update_plugin_config(plugin_name, args)
         return util.respond(204)
-
-    @jwt_required
-    def delete(self, plugin_name):
-        role.require_admin('Only admins can delete plugin software.')
-        plugins.delete_plugin_row(plugin_name)
-        return util.respond(204)
-
-    @jwt_required
-    def post(self, plugin_name):
-        role.require_admin('Only admins can create plugin software.')
-        parser = reqparse.RequestParser()
-        parser.add_argument('arguments', type=dict)
-        parser.add_argument('disabled', type=bool)
-        args = parser.parse_args()
-        plugins.insert_plugin_row(plugin_name, args)
-        return util.success()
 
 
 class APIPlugin():
