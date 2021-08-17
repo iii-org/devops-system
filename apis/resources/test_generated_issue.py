2000525e1d8ed07afe71e33b39445e5eb538660d
Romulus Urakagi Tsai <urakagi@gmail.com>
2021-08-17 10:43:44 +0800

    Change tgi format

--- apis/resources/test_generated_issue.py
+++ apis/resources/test_generated_issue.py
@@ -2,6 +2,7 @@ import json
 from datetime import datetime
 
 import model
+import nexus
 from resources import issue
 from accessories import redmine_lib
 from resources.logger import logger
@@ -37,12 +38,14 @@ def tgi_feed_sideex(row):
 
 def _handle_test_failed(project_id, software_name, filename, description,
                         branch, commit_id, result_table, result_id):
+    project_name = nexus.nx_get_project(id=project_id).name
     relation_row = model.TestGeneratedIssue.query.filter_by(
         project_id=project_id,
         software_name=software_name,
         file_name=filename
     ).first()
     if relation_row is None:
+        description = f'詳細報告請前往[測試報告列表](/#/{project_name}/scan/sideex)\n{description}'
         args = {
             'project_id': project_id,
             'tracker_id': 9,
@@ -71,15 +74,25 @@ def _handle_test_failed(project_id, software_name, filename, description,
         iss.save()
 
 
+SOFTWARE_ISSUE_TITLE = {
+    'sideex': 'SideeX',
+    'postman': 'Postman',
+    'zap': 'Zap',
+    'webinspect': 'WebInspect',
+    'sonarqube': 'SonarQube',
+    'checkmarx': 'CheckMarx'
+}
+
+
 def _get_issue_subject(filename, software_name):
     if software_name == 'postman':
         if filename == '':
             full_filename = 'postman_collection.json'
         else:
             full_filename = f'{filename}.postman_collection.json'
-        return f'{full_filename}__測試失敗'
+        return f'[{SOFTWARE_ISSUE_TITLE[software_name]}] Script: {full_filename} _測試失敗'
     else:
-        return f'{filename}__測試失敗'
+        return f'[{SOFTWARE_ISSUE_TITLE[software_name]}] Script: {filename} _測試失敗'
 
 
 def _handle_test_success(project_id, software_name, filename, description):
@@ -123,22 +136,22 @@ def tgi_create_issue(args, software_name, file_name, branch, commit_id, result_t
 def _get_postman_issue_description(row):
     now = datetime.now()
     dt_string = now.strftime("%Y-%m-%d %H:%M:%S")
-    return f'{dt_string} {row.branch} #{row.commit_id} Postman 自動化測試失敗 ({row.total - row.fail}/{row.total})'
+    return f'{dt_string} {row.branch} #{row.commit_id} 自動化測試失敗 ({row.total - row.fail}/{row.total})'
 
 
 def _get_postman_issue_close_description(row):
     now = datetime.now()
     dt_string = now.strftime("%Y-%m-%d %H:%M:%S")
-    return f'{dt_string} {row.branch} #{row.commit_id} Postman 自動化測試成功 ({row.total})'
+    return f'{dt_string} {row.branch} #{row.commit_id} 自動化測試成功 ({row.total})'
 
 
 def _get_sideex_issue_description(row, total, passed):
     now = datetime.now()
     dt_string = now.strftime("%Y-%m-%d %H:%M:%S")
-    return f'{dt_string} {row.branch} #{row.commit_id} Sideex 自動化測試失敗 ({passed}/{total})'
+    return f'{dt_string} {row.branch} #{row.commit_id} 自動化測試失敗 ({passed}/{total})'
 
 
 def _get_sideex_issue_close_description(row, total, passed):
     now = datetime.now()
     dt_string = now.strftime("%Y-%m-%d %H:%M:%S")
-    return f'{dt_string} {row.branch} #{row.commit_id} Sideex 自動化測試成功 ({total})'
+    return f'{dt_string} {row.branch} #{row.commit_id} 自動化測試成功 ({total})'
