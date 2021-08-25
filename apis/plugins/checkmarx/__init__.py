6c9bed182f08e3f4ff4addfcd2fcf9fc7a86454b
Romulus Urakagi Tsai <urakagi@gmail.com>
2021-08-25 10:44:05 +0800

    Fix Checkmarx scan fail bug

--- apis/plugins/checkmarx/__init__.py
+++ apis/plugins/checkmarx/__init__.py
@@ -100,7 +100,8 @@ class CheckMarx(object):
         status_name = status.get('name')
         if status_id in {7, 8, 9}:
             scan = Model.query.filter_by(scan_id=scan_id).one()
-            scan.stats = json.dumps(self.get_scan_statistics(scan_id))
+            if status_id != 9:
+                scan.stats = json.dumps(self.get_scan_statistics(scan_id))
             scan.scan_final_status = status_name
             db.session.commit()
         return status_id, status_name
