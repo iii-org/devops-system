6abeee31894ddebdc3717b3e0937397101c0e381
wyuchi99 <wyuchi99@gmail.com>
2021-09-11 14:50:03 +0800

    fix api server listen port

--- apis/api.py
+++ apis/api.py
@@ -632,5 +632,5 @@ def start_prod():
 
 if __name__ == "__main__":
     start_prod()
-    socketio.run(app, host='0.0.0.0', port=10030, debug=config.get('DEBUG'),
+    socketio.run(app, host='0.0.0.0', port=10009, debug=config.get('DEBUG'),
                  use_reloader=config.get('USE_RELOADER'))
