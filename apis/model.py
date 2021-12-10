6bc63c7c82efbbef8924f926328ac0436e30db0d
YuWei Chou <demoyuw@gmail.com>
2021-12-10 15:52:51 +0800

    docs (model): add description in TestResult table

--- apis/model.py
+++ apis/model.py
@@ -222,6 +222,7 @@ class TestItems(db.Model):
 
 
 class TestResults(db.Model):
+    """Postman test result table"""
     id = Column(Integer, primary_key=True)
     project_id = Column(Integer, ForeignKey(Project.id, ondelete='CASCADE'))
     branch = Column(String(50))
@@ -718,7 +719,8 @@ class CustomIssueFilter(db.Model):
     @validates("custom_filter")
     def validate_custom_filter(self, key, custom_filter):
         custom_filter_keys = sorted(list(custom_filter.keys()))
-        expected_keys = ['assigned_to_id', 'fixed_version_id', 'priority_id', 'show_closed_isssues', 'show_closed_versions', 'status_id', 'tags', 'tracker_id']
+        expected_keys = ['assigned_to_id', 'fixed_version_id', 'priority_id',
+                         'show_closed_isssues', 'show_closed_versions', 'status_id', 'tags', 'tracker_id']
         if custom_filter_keys != expected_keys:
             raise AssertionError(f"Custom filter keys must be the same as {expected_keys}.")
         return custom_filter
