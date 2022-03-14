11766c60a22aa94fe61b5c9bbc97dd729b2f8308
YuWei Chou <demoyuw@gmail.com>
2022-03-14 18:01:08 +0800

    fix: remove redurdant code.

--- apis/resources/router/__init__.py
+++ apis/resources/router/__init__.py
@@ -3,7 +3,7 @@ import json
 import model
 import plugins
 from flask_jwt_extended import get_jwt_identity
-from model import UIRoute, UIRouteUserRoleRelation, db
+from model import db
 from sqlalchemy.sql import and_
 
 
@@ -28,35 +28,3 @@ def get_plugin_software():
         if plugin is not None:
             output.append(row_to_dict(plugin))
     return output
-
-
-def get_ui_route_list():
-    return json.loads(str(UIRoute.query.all()))
-
-
-def get_user_route():
-    # Get route_name and filter by user role
-    rows = db.session.query(UIRoute).join(UIRouteUserRoleRelation, and_(
-        UIRouteUserRoleRelation.user_role == get_jwt_identity()["role_id"],
-        UIRoute.id == UIRouteUserRoleRelation.ui_route_id)).all()
-
-    # filter if plugins is not enable
-    disable_plugin_list = []
-    db_pluging_list = get_plugin_software()
-    i = 0
-    while i < len(db_pluging_list):
-        if db_pluging_list[i]["disabled"] is False:
-            del db_pluging_list[i]
-        else:
-            i += 1
-    for db_pluging_dict in db_pluging_list:
-        if db_pluging_dict['name'] in plugins.list_plugin_modules() and \
-                hasattr(getattr(plugins, db_pluging_dict['name']), "ui_route") is True:
-            disable_plugin_list.extend(getattr(plugins, db_pluging_dict['name']).ui_route)
-    j = 0
-    while j < len(rows):
-        if rows[j].route_name in disable_plugin_list:
-            del rows[j]
-        else:
-            j += 1
-    return json.loads(str(rows))
--- apis/urls/router/view.py
+++ apis/urls/router/view.py
@@ -19,21 +19,3 @@ class Router(Resource):
         except DevOpsError:
             return util.respond(404, get_router_error
                                 )
-
-
-@ doc(tags=['Router'], description="Get UI route name")
-@ marshal_with(route_model.UIRouteListResponse)
-class RouterNameV2(MethodResource):
-
-    @ jwt_required
-    def get(self):
-        util.require_admin()
-        return util.success(router.get_ui_route_list())
-
-
-@ doc(tags=['Router'], description="Get the user route")
-@ marshal_with(route_model.UIRouteListResponse)
-class UserRouteV2(MethodResource):
-    @ jwt_required
-    def get(self):
-        return util.success(router.get_user_route())
