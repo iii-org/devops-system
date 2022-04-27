d75686f0e6c5d4511669ddce1cb316f67c76ddb2
john19968010 <john19968010@gmail.com>
2022-04-27 15:42:46 +0800

    [Refactor][Project] Only check server alive during delete project

--- apis/plugins/cmas/__init__.py
+++ apis/plugins/cmas/__init__.py
@@ -10,3 +10,5 @@ def router(api, add_resource):
     api.add_resource(cmas_main.CMASRemote, '/cmas/<string:task_id>')
     api.add_resource(cmas_main.CMASDonwload, '/cmas/<string:task_id>/<string:file_type>')
     api.add_resource(cmas_main.CMASSecret, '/cmas/secret')
+    api.add_resource(cmas_main.CMASAPKREmove, '/cmas/apk_remove')
+
