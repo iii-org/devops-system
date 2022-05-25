546c8f30ea5bce081eb0dd1f5765cd9e7f3dc176
Joseph Huang <pubyuche@gmail.com>
2022-05-25 14:42:19 +0800

    Add swagger UI server with basePath

--- apis/api.py
+++ apis/api.py
@@ -82,7 +82,8 @@ app.config.update({
         version='v1',
         plugins=[MarshmallowPlugin()],
         securityDefinitions=security_definitions,
-        openapi_version='2.0.0'
+        openapi_version='2.0.0',
+        basePath='/prod-api'
     ),
     'APISPEC_SWAGGER_URL': '/swagger/',  # URI to access API Doc JSON
     'APISPEC_SWAGGER_UI_URL': '/swagger-ui/'  # URI to access UI of API Doc
