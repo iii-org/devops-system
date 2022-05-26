b511919f8dc6d1a4a51d6fd1ce320e61b77684a5
YuWei Chou <demoyuw@gmail.com>
2022-05-26 11:44:49 +0800

    chore (harbor scan): Edit openapi doc

--- apis/urls/harbor/router_model.py
+++ apis/urls/harbor/router_model.py
@@ -3,10 +3,10 @@ from marshmallow import Schema, fields
 
 class CreateHarborScan(Schema):
     branch = fields.Str(required=True, description='Branch name', example="master")
-    commit_id = fields.Str(required=True, description='commit short id',
+    commit_id = fields.Str(required=True, description='Commit short id',
                            example="d45736e4")
 
 
 class HarborScanList(Schema):
-    per_page = fields.Int(required=False, description='Branch name', example="master")
-    page = fields.Int(required=False, description='commit short id', example="d45736e4")
+    per_page = fields.Int(required=False, description='Show how many items at one page', example="10")
+    page = fields.Int(required=False, description='Page number', example="1")
