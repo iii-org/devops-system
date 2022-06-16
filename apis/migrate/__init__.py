9ed4e1dd252e905c4d436f53f5b1d766373dcf96
john19968010 <john19968010@gmail.com>
2022-06-16 15:41:51 +0800

    [Refactor][Migrate] Insert default data in UserMessageType

--- apis/migrate/__init__.py
+++ apis/migrate/__init__.py
@@ -6,7 +6,7 @@ import uuid
 from migrate.upgrade_function.upload_file_types import upload_file_types
 from model import db, ProjectPluginRelation, Project, UserPluginRelation, User, ProjectUserRole, PluginSoftware, \
     DefaultAlertDays, TraceOrder, TraceResult, Application, IssueExtensions, Lock, RedmineProject, ServerType, SystemParameter, \
-    ProjectResourceStoragelevel
+    ProjectResourceStoragelevel, UserMessageType
 from plugins.sonarqube.sonarqube_main import sq_create_project, sq_create_user
 from resources import harbor, kubernetesClient, role, devops_version
 from resources.apiError import DevOpsError
@@ -40,7 +40,7 @@ VERSIONS = ['0.9.2', '0.9.2.1', '0.9.2.2', '0.9.2.3', '0.9.2.4', '0.9.2.5',
             '1.15.0.13', '1.15.0.14', '1.15.0.15', '1.15.0.16', '1.15.1.0', '1.15.1.1', '1.15.2.0', '1.15.2.1', '1.15.2.2', '1.15.2.3', '1.15.2.4',
             '1.16.0.1', '1.16.1.0', '1.16.1.1', '1.16.1.2', '1.16.1.3', '1.16.1.4', '1.16.1.5', '1.16.2.0', '1.16.2.1',
             '1.16.2.2', '1.16.2.3', '1.16.2.4', '1.16.2.5', '1.16.2.6', '1.16.2.7', '1.16.3.0', '1.16.3.1', '1.17.1.0', '1.17.1.1', '1.17.2.1',
-            '1.17.2.2', '1.17.2.3', '1.17.2.4', '1.17.2.5', '1.17.2.6', '1.17.2.7', '1.17.2.8']
+            '1.17.2.2', '1.17.2.3', '1.17.2.4', '1.17.2.5', '1.17.2.6', '1.17.2.7', '1.17.2.8', '1.17.2.9']
 ONLY_UPDATE_DB_MODELS = [
     '0.9.2.1', '0.9.2.2', '0.9.2.3', '0.9.2.5', '0.9.2.6', '0.9.2.a8',
     '1.0.0.2', '1.3.0.1', '1.3.0.2', '1.3.0.3', '1.3.0.4', '1.3.1', '1.3.1.1', '1.3.1.2',
@@ -204,21 +204,24 @@ def upgrade(version):
         insert_gitlab_condition_in_pj_rs_stg_level()
     elif version == '1.17.2.7':
         pass
+    elif version == '1,17.2.9':
+        insert_default_info_in_user_message_type()
 
 
-# def insert_default_info_in_user_notify_type():
-#     for user in User.query.all():
-#         if not user.login.startswith("project_bot"):
-#             user_id = user.id
-#             user_notify_type = UserNotifyType.query.filter_by(user_id=user_id).first()
-#             if user_notify_type is None:
-#                 row = UserNotifyType(
-#                     user_id=user_id,
-#                     notification=True,
-#                     mail=False
-#                 )
-#                 db.session.add(row)
-#                 db.session.commit()
+def insert_default_info_in_user_message_type():
+    for user in User.query.all():
+        if not user.login.startswith("project_bot"):
+            user_id = user.id
+            user_notify_type = UserMessageType.query.filter_by(user_id=user_id).first()
+            if user_notify_type is None:
+                row = UserMessageType(
+                    user_id=user_id,
+                    notification=True,
+                    mail=False,
+                    teams=False
+                )
+                db.session.add(row)
+                db.session.commit()
 
 
 def insert_gitlab_condition_in_pj_rs_stg_level():
