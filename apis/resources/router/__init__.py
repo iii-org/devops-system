c091abe0bc76479775e84985a00d1e40f55c94ba
YuWei Chou <demoyuw@gmail.com>
2022-07-05 09:48:19 +0800

    chore (ui route): remove redundant code

--- apis/resources/router/__init__.py
+++ apis/resources/router/__init__.py
@@ -11,26 +11,6 @@ from sqlalchemy.sql import and_
 key_return_json = ['parameter']
 
 
-def load_ui_route():
-    ui_route_json = util.read_json_file("apis/ui_route.json")
-    row = UIRouteJson.query.filter_by(name='ui_route').first()
-    if row is None:
-        new_row = UIRouteJson(
-            name='ui_route',
-            ui_route=ui_route_json,
-            created_at=datetime.datetime.utcnow(),
-            updated_at=datetime.datetime.utcnow()
-        )
-        db.session.add(new_row)
-        db.session.commit()
-    elif str(ui_route_json) != str(row.ui_route):
-        row.ui_route = ui_route_json
-        row.updated_at = datetime.datetime.utcnow()
-        db.session.commit()
-    else:
-        print("Noting change")
-
-
 def row_to_dict(row):
     ret = {}
     if row is None:
