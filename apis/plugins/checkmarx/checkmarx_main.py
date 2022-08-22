603bb7228ef5b41659304e96fd894c9dbff2a13d
roy0001919 <roy0001919@gmail.com>
2022-08-22 14:29:35 +0800

    Fix: checkmarx to_json report_ready

--- apis/plugins/checkmarx/checkmarx_main.py
+++ apis/plugins/checkmarx/checkmarx_main.py
@@ -318,7 +318,7 @@ class CheckMarx(object):
             'stats': stats,
             'run_at': str(row.run_at),
             'report_id': row.report_id,
-            'report_ready': row.finished is True
+            'report_ready': row.finished is True and row.report_id != -1
         }
 
 
