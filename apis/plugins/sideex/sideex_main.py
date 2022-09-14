2933a6f6dbbe8beb2e84541ec95a61fab4a08fd4
roy0001919 <roy0001919@gmail.com>
2022-09-14 14:46:20 +0800

    Feat: update file name with user_id

--- apis/plugins/sideex/sideex_main.py
+++ apis/plugins/sideex/sideex_main.py
@@ -22,6 +22,7 @@ import resources.apiError as apiError
 import pandas as pd
 import subprocess
 import urllib.parse
+from flask_jwt_extended import get_jwt_identity
 
 
 def sd_start_test(args):
@@ -235,7 +236,7 @@ def get_global_json(project_id, filename):
             if k in variables_data.keys():
                 result_dict.update({k: variables_data[k]})
             else:
-                result_dict.update({k: None})
+                result_dict.update({k: []})
         return result_dict
 
 
@@ -243,7 +244,7 @@ def get_setting_file(project_id, filename):
     result_list = []
     result_dict = get_global_json(project_id, filename)
     output_list = get_sideex_json_variable(project_id, filename)
-    setting_data = get_gitlab_file_todict(project_id, '_setting_sideex.json')
+    setting_data = get_gitlab_file_todict(project_id, f'_{get_jwt_identity()["user_id"]}-setting_sideex.json')
     if setting_data and type(setting_data) == str:
         setting_data = json.loads(setting_data)
     sorted_dict = {}
@@ -254,7 +255,7 @@ def get_setting_file(project_id, filename):
             if k in sorted_dict.keys():
                 result_dict.update({k: sorted_dict[k]})
             else:
-                result_dict.update({k: None})
+                result_dict.update({k: []})
     if result_dict:
         result_list = [{"name": k, "type": str(type(v[0])).replace('<class \'', '').replace('\'>', '') if v != [] else None, "value": v} for k, v in result_dict.items()]
     return_dict = {
@@ -268,7 +269,7 @@ def save_to_txt(kwargs):
     df = pd.DataFrame(kwargs['var'])
     df['name'] = df['name'].apply(lambda x: x + ':')
     df['value'] = df['value'].apply(lambda x: str(x).replace('[', '').replace(']', '').replace("\'", ''))
-    np.savetxt('./iiidevops/sideex/_model.txt', df[['name', 'value']].values, fmt='%s')
+    np.savetxt(f"./iiidevops/sideex/_{get_jwt_identity()['user_id']}-model.txt", df[['name', 'value']].values, fmt='%s')
 
 
 def check_variable_not_null(kwargs):
@@ -282,7 +283,7 @@ def check_variable_not_null(kwargs):
 def update_config_file(project_id, kwargs):
     check_variable_not_null(kwargs)
     f = False
-    filename = '_setting_sideex.json'
+    filename = f'_{get_jwt_identity()["user_id"]}-setting_sideex.json'
     paths = [{
         "software_name": "SideeX",
         "path": "iiidevops/sideex",
@@ -292,14 +293,14 @@ def update_config_file(project_id, kwargs):
         nexus_project_id=project_id).git_repository_id
     if not os.path.isdir('./iiidevops/sideex'):
         Path('./iiidevops/sideex').mkdir(parents=True, exist_ok=True)
-    with open('./iiidevops/sideex/_setting_sideex.json', "w+") as json_data:
+    with open(f'./iiidevops/sideex/_{get_jwt_identity()["user_id"]}-setting_sideex.json', "w+") as json_data:
         json_data.write(json.dumps(kwargs))
     save_to_txt(kwargs)
     pj = gitlab.gitlab.gl.projects.get(repository_id)
-    if get_gitlab_file_todict(project_id, '_model.txt'):
-        url = urllib.parse.quote("iiidevops/sideex/_model.txt", safe='')
+    if get_gitlab_file_todict(project_id, f"{get_jwt_identity()['user_id']}_model.txt"):
+        url = urllib.parse.quote(f"iiidevops/sideex/_{get_jwt_identity()['user_id']}-model.txt", safe='')
         gitlab.gitlab.gl_delete_file(repository_id, url, {"commit_message": "delete _model.txt by sideex_auto_test"}, "master")
-    gitlab.gitlab.gl_create_file(pj, "iiidevops/sideex/_model.txt", "_model.txt", "./iiidevops/sideex", "master")
+    gitlab.gitlab.gl_create_file(pj, f"iiidevops/sideex/_{get_jwt_identity()['user_id']}-model.txt", f"_{get_jwt_identity()['user_id']}-model.txt", "./iiidevops/sideex", "master")
     for path in paths:
         trees = gitlab.gitlab.ql_get_tree(repository_id, path['path'])
         for tree in trees:
@@ -313,12 +314,12 @@ def update_config_file(project_id, kwargs):
                     commit_message=f'Add "iiidevops" in branch.rancher-pipeline.yml.')
                 break
         if not f:
-            gitlab.gitlab.gl_create_file(pj, "iiidevops/sideex/_setting_sideex.json", "_setting_sideex.json",
+            gitlab.gitlab.gl_create_file(pj, f"iiidevops/sideex/_{get_jwt_identity()['user_id']}-setting_sideex.json", f"_{get_jwt_identity()['user_id']}-setting_sideex.json",
                                                "./iiidevops/sideex", "master")
 
 
 def pict_convert_result():
-    if os.path.isfile('./iiidevops/sideex/_model.txt'):
+    if os.path.isfile(f"./iiidevops/sideex/_{get_jwt_identity()['user_id']}-model.txt"):
         # std_output = subprocess.check_output(['pict', 'iiidevops/sideex/_model.txt'])
         std_output = b'abc\tdef\txx2\n10\ta54\t12\n123\tabc\t12\n123\ta54\tab\n3\tabc\t99\n10\tabc\t56\n3\txyz\tab\n3\txyz\t99\n3\ta54\t12\n10\txyz\tab\n123\txyz\t99\n10\ta54\t99\n2\tabc\t99\n123\ta54\t56\n3\tabc\tab\n2\txyz\t12\n2\ta54\t56\n3\txyz\t56\n3\ta54\t12\n2\txyz\tab\n3\tabc\t56\n'
         remove_space = std_output.decode("ascii").split('\t')
@@ -327,12 +328,12 @@ def pict_convert_result():
         remove_n.remove('')
         return remove_n
     else:
-        raise apiError.DevOpsError(404, "_model.txt not found")
+        raise apiError.DevOpsError(404, f"_{get_jwt_identity()['user_id']}-model.txt not found")
 
 
 def sort_convert_result_to_df():
     pict_list = pict_convert_result()
-    file = open('./iiidevops/sideex/_model.txt', 'r')
+    file = open(f"./iiidevops/sideex/_{get_jwt_identity()['user_id']}-model.txt", 'r')
     txt_content = file.read()
     cut_num = txt_content.count('\n')
     df_input = pd.DataFrame(pict_list)
@@ -355,9 +356,9 @@ def gernerate_json_file(filename):
     for i in range(1, len(df_sorted)):
         for key, value in df_sorted.T.to_dict()[i].items():
             result = re.sub('\${%s\}' % key, value, json.dumps(txt_content, indent=4))
-            with open(f'./iiidevops/sideex/*sideex{i}.json', 'w') as json_writer:
+            with open(f'./iiidevops/sideex/*{get_jwt_identity()["user_id"]}-sideex{i}.json', 'w') as json_writer:
                 json_writer.write(result)
-                file = open(f'./iiidevops/sideex/*sideex{i}.json', 'r')
+                file = open(f'./iiidevops/sideex/*{get_jwt_identity()["user_id"]}-sideex{i}.json', 'r')
                 txt_content = json.loads(file.read())
         file = open(f'./iiidevops/sideex/{filename}', 'r')
         txt_content = json.loads(file.read())
