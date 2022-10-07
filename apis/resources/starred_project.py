5e747400ad20875bdb797f9c6b26e8d70f0ed61f
Allen <s96016641@gmail.com>
2022-10-07 10:54:23 +0800

    Feat: Add stared project limit to 10

--- apis/resources/starred_project.py
+++ apis/resources/starred_project.py
@@ -5,20 +5,32 @@ import model
 import nexus
 import util
 from resources import role
+from resources.apiError import DevOpsError, maximum_error
 
 
 def spj_set(user_id, project_id):
-    row = model.StarredProject.query.filter_by(user_id=user_id, project_id=project_id).first()
+    row = model.StarredProject.query.filter_by(
+        user_id=user_id, project_id=project_id
+    ).first()
     if row:
         # Already has the record
         return
+    if model.StarredProject.query.filter_by(user_id=user_id).count() >= 10:
+        # Limit to 10
+        raise DevOpsError(
+            400,
+            f"User {user_id} can not added {project_id} to starred projects, reached the limit of 10",
+            error=maximum_error("Stared projects", 10),
+        )
     new = model.StarredProject(user_id=user_id, project_id=project_id)
     model.db.session.add(new)
     model.db.session.commit()
 
 
 def spj_unset(user_id, project_id):
-    row = model.StarredProject.query.filter_by(user_id=user_id, project_id=project_id).first()
+    row = model.StarredProject.query.filter_by(
+        user_id=user_id, project_id=project_id
+    ).first()
     if not row:
         # Already does not have the record
         return
