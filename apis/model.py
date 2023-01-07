814498e648cb26bc4ca1f141cbd5988c62dedad4
john19968010 <john19968010@gmail.com>
2023-01-07 17:30:37 +0800

    [Feat][Model] Add table PipelineExecution (#4816)

--- apis/model.py
+++ apis/model.py
@@ -1135,3 +1135,11 @@ class Pict(db.Model):
     run_at = Column(DateTime)
     status = Column(String)
     sideex_id = Column(Integer, ForeignKey(Sideex.id, ondelete='CASCADE'), nullable=False)
+
+
+class PipelineExecution(db.Model):
+    id = Column(Integer, primary_key=True)
+    project_id = Column(Integer, ForeignKey(Project.id, ondelete='CASCADE'))
+    branch = Column(String)
+    commit_id = Column(String)
+    run_branches = Column(postgresql.ARRAY(String))
