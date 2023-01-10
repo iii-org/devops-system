01e53e2ef067a7ea8e0d5d06fd5af723e744e9c4
john19968010 <john19968010@gmail.com>
2023-01-10 14:02:44 +0800

    [Feat] Pipeline execution 2nd ver

--- apis/urls/rancher/view.py
+++ apis/urls/rancher/view.py
@@ -6,14 +6,11 @@ from nexus import nx_get_project_plugin_relation
 from . import router_model
 
 
-
 class CheckPipeline(MethodResource):
-    @doc(tags=['Rancher'], description='Check rancher need run or not.')
+    @doc(tags=["Rancher"], description="Check rancher need run or not.")
     @use_kwargs(router_model.CheckPipelineSchema, location="form")
     def post(self, **kwargs):
         check_pipeline_need_remove(
-            kwargs["repo_name"],
-            kwargs["branch"],
-            kwargs["commit_id"]
+            kwargs["repo_name"], kwargs["branch"], kwargs["commit_id"]
         )
         return util.success()
