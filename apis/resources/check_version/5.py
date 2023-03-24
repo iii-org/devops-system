9f2be6cb9c132223bcb9fb7802d8038ab0c04016
LanI <lani0001@gmail.com>
2023-03-24 16:13:37 +0800

    #4997 update template to 1.27

---
+++ apis/resources/check_version/5.py
@@ -0,0 +1,9 @@
+import config
+
+# Runner answer & version
+RUNNER_VERSION_MAPPING = {"sonarqube": {"version": "0.3.1"}, "checkmarx": {"version": "0.3.1"}}
+
+# Image version
+IMAGE_VERSION_MAPPING = {}
+
+API_VERSION = "1.27.0"
