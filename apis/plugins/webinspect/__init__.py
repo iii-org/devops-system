b78f6ad2927adf152099112c034f46b3a13d9c90
john19968010 <john19968010@gmail.com>
2023-03-29 16:10:51 +0800

    [Feat][WIE] Add Basic CRUD APIs

--- apis/plugins/webinspect/__init__.py
+++ apis/plugins/webinspect/__init__.py
@@ -1,16 +1,15 @@
-from . import webinspect_main
+from . import webinspect_main_real
 
-ui_route = ["Webinspect", "WebInspectReport"]
+ui_route = ["WebInspect"]
+
 # --------------------- API router ---------------------
 
 
 def router(api, add_resource):
     # WebInspect
-    api.add_resource(
-        webinspect_main.WebInspectScan,
-        "/webinspect/create_scan",
-        "/webinspect/list_scan/<project_name>",
-    )
-    api.add_resource(webinspect_main.WebInspectScanStatus, "/webinspect/status/<scan_id>")
-    api.add_resource(webinspect_main.WebInspectScanStatistics, "/webinspect/stats/<scan_id>")
-    api.add_resource(webinspect_main.WebInspectReport, "/webinspect/report/<scan_id>")
+    api.add_resource(webinspect_main_real.WebInspectPostScan, "/webinspect/scan")
+    add_resource(webinspect_main_real.WebInspectPostScan, "private")
+    api.add_resource(webinspect_main_real.WebInspectListScan, "/project/<sint:project_id>/webinspect/scan")
+    add_resource(webinspect_main_real.WebInspectListScan, "private")
+    api.add_resource(webinspect_main_real.WebInspectScan, "/webinspect/scan/<s_id>")
+    add_resource(webinspect_main_real.WebInspectScan, "private")
---
+++ apis/plugins/webinspect/router_model.py
@@ -0,0 +1,81 @@
+from marshmallow import Schema, fields
+from util import CommonBasicResponse
+
+
+#################################### Schema ####################################
+
+
+class WIEScanPostSchema(Schema):
+    scan_id = fields.Str(required=True, example="1234")
+    project_name = fields.Str(required=True, example="project_name")
+    branch = fields.Str(required=True, example="master")
+    commit_id = fields.Str(required=True, example="#77777")
+
+
+class WIEScanGetSchema(Schema):
+    limit = fields.Int(doc="limit", example=1)
+    offset = fields.Int(doc="offset", example=1)
+
+
+class WIEScanUpdateSchema(Schema):
+    scan_id = fields.Str(example="1234")
+    status = fields.Str(example="Complete")
+    finished = fields.Bool(example=True)
+    finished_at = fields.DateTime(example="1970-01-01T00:00:00")
+
+
+#################################### Response ####################################
+
+
+class SbomGetRes(CommonBasicResponse):
+    data = fields.List(
+        fields.Dict(
+            example={
+                "branch": "master",
+                "commit": "Z7777777",
+                "commit_url": "https://",
+                "created_at": "1970-01-01T00:00:00",
+                "finished": True,
+                "finished_at": "1970-01-01T00:00:00",
+                "id": 1,
+                "logs": "",
+                "package_nums": 1,
+                "project_id": 1,
+                "scan_overview": {},
+                "scan_status": "Finished",
+            }
+        )
+    )
+
+
+class SbomPostRes(Schema):
+    id = fields.Int(required=True)
+
+
+class SbomListResponse(Schema):
+    per_page = fields.Int(required=False, description="Show how many items at one page", example="10")
+    page = fields.Int(required=False, description="Page number", example="1")
+    search = fields.Str(required=False, description="params", example="master")
+
+
+class SbomDetailResponse(Schema):
+    per_page = fields.Int(required=False, description="Show how many items at one page", example="10")
+    page = fields.Int(required=False, description="Page number", example="1")
+    sort = fields.Str(required=False, description="sort", example="versions")
+    ascending = fields.Boolean(required=False, description="ascending", example=True)
+
+
+class SbomGetSbomsResponse(Schema):
+    latest = fields.Boolean()
+
+
+class SbomGetFileList(CommonBasicResponse):
+    data = fields.List(fields.Str())
+
+
+class SbomGetProjectID(Schema):
+    project_id = fields.Int(example=3)
+
+
+class SbomGetSbomID(Schema):
+    sbom_id = fields.Int(example=3)
