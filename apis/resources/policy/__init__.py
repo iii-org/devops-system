ddf109bfc6be6ab9041f8f4b49f7d2fb23cb48d2
LanI <lani0001@gmail.com>
2023-04-19 14:38:30 +0800

    [Fix] DB password policy error

--- apis/resources/policy/__init__.py
+++ apis/resources/policy/__init__.py
@@ -44,32 +44,32 @@ DB_POLICY = dict({
 
 
 def check_pswd_policy(db_type, db_user, db_pswd):
-    db_policy = DB_POLICY[db_type]
-    if db_policy:
-        output = {"pass": True}
-        if db_policy["RE"]:
-            result = re.match(db_policy["RE"], db_pswd)
-            if result is None:
-                output["pass"] = False
-                output["description"] = db_policy["Policy"]
-            if db_type == "MSSQL" or db_type == "postgreSQL":
-                if db_user in db_pswd:
-                    output["pass"] = False
-                    output["description"] = db_policy["Policy"]
-        return output
-    return None
+	db_policy = DB_POLICY.get(db_type)
+	if db_policy:
+		output = {"pass": True}
+		if db_policy["RE"]:
+			result = re.match(db_policy["RE"], db_pswd)
+			if result is None:
+				output["pass"] = False
+				output["description"] = db_policy["Policy"]
+			if db_type == "MSSQL" or db_type == "postgreSQL":
+				if db_user in db_pswd:
+					output["pass"] = False
+					output["description"] = db_policy["Policy"]
+		return output
+	return None
 
 
 class DBPSWDPolicy(Resource):
-    @jwt_required()
-    def post(self):
-        parser = reqparse.RequestParser()
-        parser.add_argument("db_type", type=str, required=True)
-        parser.add_argument("db_user", type=str, required=True)
-        parser.add_argument("db_pswd", type=str, required=True)
-        args = parser.parse_args()
-        output = check_pswd_policy(args.get("db_type"), args.get("db_user"), args.get("db_pswd"))
-        if output:
-            return util.success(output)
-        else:
-            return util.respond(404, f'{args.get("db_type")}')
+	@jwt_required()
+	def post(self):
+		parser = reqparse.RequestParser()
+		parser.add_argument("db_type", type=str, required=True)
+		parser.add_argument("db_user", type=str, required=True)
+		parser.add_argument("db_pswd", type=str, required=True)
+		args = parser.parse_args()
+		output = check_pswd_policy(args.get("db_type"), args.get("db_user"), args.get("db_pswd"))
+		if output:
+			return util.success(output)
+		else:
+			return util.respond(404, f'db_type {args.get("db_type")} not found.')
