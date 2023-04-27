598e2bfc29e540ee72d7ff43b9c5d0109bb93ca9
LanI <lani0001@gmail.com>
2023-04-27 15:30:11 +0800

    [Fix] checkmarx_main.py (#4984)

--- apis/plugins/checkmarx/checkmarx_main.py
+++ apis/plugins/checkmarx/checkmarx_main.py
@@ -505,9 +505,6 @@ def checkamrx_keep_report(repo_id, keep_record:int = 5):
     if rows:
         report_count = 0
         for row in rows:
-            # 解決 Instance is not bound to a Session 的錯誤
-            if row not in db.session:
-                row = db.session.merge(row)
             # 原始的pdf檔可能已經失效,將scan_final_status改成null後,將觸發前端重新去要pdf檔
             # 最近30天內及最新的五筆
             if report_count < keep_record and utcnow - datetime.timedelta(days=30) <= row.run_at:
@@ -518,8 +515,11 @@ def checkamrx_keep_report(repo_id, keep_record:int = 5):
                     if status_id == 10:
                         status_id, _ = 2, "PreScan"
                     logger.logger.info(f"scan_id: {row.scan_id}, status_id: {status_id}, ststus_name: {_}")
-                    # 因為在 get_scan_status() 中有重新跟資料庫取 row 同一筆資料並且會更新資料，故這邊要再重新取一次
-                    row = Model.query.filter_by(scan_id=row.scan_id).one()
+                    # # 因為在 get_scan_status() 中有重新跟資料庫取 row 同一筆資料並且會更新資料，故這邊要再重新取一次
+                    # row = Model.query.filter_by(scan_id=row.scan_id).one()
+                    # 解決 Instance is not bound to a Session 的錯誤
+                    if row not in db.session:
+                        row = db.session.merge(row)
                     if row.report_id is None:
                         row.report_id = -1
                         logger.logger.info(f"Updating checkmarx scan: {row.scan_id}'s report_id {row.report_id}")
