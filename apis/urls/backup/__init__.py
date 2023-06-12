2eab6f2f8b982c257140f71ec373ed5ab9de375c
LanI <lani0001@gmail.com>
2023-06-12 16:26:20 +0800

    [FEAT] add backup project data to backup_project.json (#5502)

--- apis/urls/backup/__init__.py
+++ apis/urls/backup/__init__.py
@@ -4,3 +4,6 @@ from . import view
 def backup_url(api, add_resource):
     api.add_resource(view.UserBackupToJsonV2, "/v2/user/backup/json")
     add_resource(view.UserBackupToJsonV2, "public")
+
+    api.add_resource(view.ProjectBackupToJsonV2, "/v2/project/backup/json")
+    add_resource(view.ProjectBackupToJsonV2, "public")
--- apis/urls/backup/view.py
+++ apis/urls/backup/view.py
@@ -1,14 +1,23 @@
 from flask_jwt_extended import jwt_required
 from flask_apispec import marshal_with, doc, use_kwargs
 from flask_apispec.views import MethodResource
-from .control import backup_user_to_json
+from .control import backup_user_to_json, backup_project_to_json
 import util
 
 
-@doc(tags=["User"], description="User's data backup to json file")
+@doc(tags=["Backup"], description="User's data backup to json file")
 class UserBackupToJsonV2(MethodResource):
-    # @marshal_with(util.CommonResponse)
+    @marshal_with(util.CommonResponse)
     @jwt_required()
-    def post(self, **kwargs):
+    def post(self):
         backup_user_to_json()
+        return util.success()
+
+
+@doc(tags=["Backup"], description="Project's data backup to json file")
+class ProjectBackupToJsonV2(MethodResource):
+    @marshal_with(util.CommonResponse)
+    @jwt_required()
+    def post(self):
+        backup_project_to_json()
         return util.success()
