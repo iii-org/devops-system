b334dabd5347e95d89aa2167c1d211a63abeae5b
allen0099 <allen0099@sudo.host>
2023-06-15 10:37:59 +0800

    🐛 Fix entry bug

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 
 # If arguments are passed to docker, run them instead
 if [ ! "$#" -gt 0 ]; then
-  PYTHONPATH=/usr/src/app gunicorn --worker-class eventlet -w 1 'apis.api:start_prod()' --timeout 120 --threads 1 -b 0.0.0.0:10009
+  PYTHONPATH=/usr/src/app/apis gunicorn --worker-class eventlet -w 1 'apis.api:start_prod()' --timeout 120 --threads 1 -b 0.0.0.0:10009
 fi
 
 exec "$@"
