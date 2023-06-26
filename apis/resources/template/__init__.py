a423128cdb6de2f6310dd05a325b637980851901
LanI <lani0001@gmail.com>
2023-06-26 17:35:38 +0800

    update update_stage_when_plugin_disable function (#5885)

--- apis/resources/template/__init__.py
+++ apis/resources/template/__init__.py
@@ -315,7 +315,12 @@ def fetch_and_update_template_cache():
 def __update_stage_when_plugin_disable(stage):
     if stage.get("iiidevops") is not None:
         for software in support_software_json:
-            if software.get("template_key") == stage.get("iiidevops") and software.get("plugin_disabled") is True:
+            logger.logger.info(f'template_key:[{software.get("template_key")}], iiidevops:[{stage.get("iiidevops")}]' +
+                               f'plugin_disabled:[{software.get("plugin_disabled")}]')
+            if (software.get("template_key") == stage.get("iiidevops") and
+                    ((stage.get("iiidevops") == "sonarqube" and software.get("plugin_disabled") is True) or
+                     (stage.get("iiidevops") != "sonarqube")
+                    )):
                 if "when" not in stage:
                     stage["when"] = {"branch": {"include": []}}
                 stage_when = stage.get("when", {}).get("branch", {}).get("include", {})
