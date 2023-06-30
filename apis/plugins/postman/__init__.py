45e3dc9cfcb2c80d0dc30dac9a52fa63741fe808
john19968010 <john19968010@gmail.com>
2023-06-30 11:47:36 +0800

    no change v2

--- apis/plugins/postman/__init__.py
+++ apis/plugins/postman/__init__.py
@@ -8,11 +8,11 @@ ui_route = ["Postman", "PostmanTestCase"]
 
 def router(api, add_resource):
     # Postman tests
-    api.add_resource(postman_main.ExportToPostmanV2, "/export_to_postman/<sint:project_id>")
+    api.add_resource(postman_main.ExportToPostmanV2, "/v2/export_to_postman/<sint:project_id>")
     add_resource(postman_main.ExportToPostmanV2, "public")
-    api.add_resource(postman_main.PostmanResultsV2, "/postman_results/<sint:project_id>")
+    api.add_resource(postman_main.PostmanResultsV2, "/v2/postman_results/<sint:project_id>")
     add_resource(postman_main.PostmanResultsV2, "public")
-    api.add_resource(postman_main.PostmanScanReportV2, "/postman_report/<int:id>")
+    api.add_resource(postman_main.PostmanScanReportV2, "/v2/postman_report/<int:id>")
     add_resource(postman_main.PostmanScanReportV2, "public")
-    api.add_resource(postman_main.PostmanReportV2, "/testResults")
+    api.add_resource(postman_main.PostmanReportV2, "/v2/testResults")
     add_resource(postman_main.PostmanReportV2, "public")
