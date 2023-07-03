dfd507633b40a8e8ede73a2f5c5e19ff875cd72f
LanI <lani0001@gmail.com>
2023-07-03 17:50:54 +0800

    modified backup and restore user and project API need jwt (#5502)

--- apis/urls/backup/view.py
+++ apis/urls/backup/view.py
@@ -1,4 +1,4 @@
-from resources.handler.jwt import jwt_required
+from resources.handler.jwt import jwt_required_cronjob
 from flask_apispec import marshal_with, doc, use_kwargs
 from flask_apispec.views import MethodResource
 from .control import backup_user_to_json, backup_project_to_json
@@ -8,7 +8,7 @@ import util
 @doc(tags=["Backup"], description="User's data backup to json file")
 class UserBackupToJsonV2(MethodResource):
     @marshal_with(util.CommonResponse)
-    # @jwt_required
+    @jwt_required_cronjob
     def post(self, **kwargs):
         backup_user_to_json()
         return util.success()
@@ -17,7 +17,7 @@ class UserBackupToJsonV2(MethodResource):
 @doc(tags=["Backup"], description="Project's data backup to json file")
 class ProjectBackupToJsonV2(MethodResource):
     @marshal_with(util.CommonResponse)
-    # @jwt_required
+    @jwt_required_cronjob
     def post(self):
         backup_project_to_json()
         return util.success()
--- apis/urls/restore/view.py
+++ apis/urls/restore/view.py
@@ -1,4 +1,4 @@
-from resources.handler.jwt import jwt_required
+from resources.handler.jwt import jwt_required_cronjob
 from flask_apispec import marshal_with, doc, use_kwargs
 from flask_apispec.views import MethodResource
 from .control import restore_user_from_json, restore_project_from_json
@@ -8,7 +8,7 @@ import util
 # @doc(tags=["Restore"], description="User's data restore from json file")
 class UserRestoreFromJsonV2(MethodResource):
     # @marshal_with(util.CommonResponse)
-    # @jwt_required
+    @jwt_required_cronjob
     def post(self):
         restore_user_from_json()
         return util.success()
@@ -17,7 +17,7 @@ class UserRestoreFromJsonV2(MethodResource):
 # @doc(tags=["Restore"], description="Project's data restore from json file")
 class ProjectRestoreFromJsonV2(MethodResource):
     # @marshal_with(util.CommonResponse)
-    # @jwt_required
+    @jwt_required_cronjob
     def post(self):
         restore_project_from_json()
         return util.success()
